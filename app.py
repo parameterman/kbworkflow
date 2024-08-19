0ae6a0f8654daf04dedde81c52417045c34716a2
Jing-yilin <yilin.jing.ai@outlook.com>
2024-08-19 17:38:19 +0800

    ✨ feat: 增加参数启动

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 import logging
 from datetime import datetime
 from rich.logging import RichHandler
-
+import argparse
 
 def setup_logging():
     log_dir = "logs"
@@ -19,11 +19,9 @@ def setup_logging():
         handlers=[RichHandler(), logging.FileHandler(log_file)],
     )
 
-
 setup_logging()
 
 logger = logging.getLogger(__name__)
-
 
 def save_uploaded_file(uploaded_file):
     try:
@@ -34,32 +32,41 @@ def save_uploaded_file(uploaded_file):
         st.error(f"Failed to save uploaded file: {e}")
         return None
 
-
 def load_config(file_path):
     with open(file_path, "r") as file:
         return yaml.safe_load(file)
 
-
 def run_app(config):
     AppFactory.create(config=config["app"]).run()
 
+def main(config_path=None):
+    # st.title("Wisup Configuration Runner")
 
-def main():
-    st.title("Wisup Configuration Runner")
-
-    uploaded_file = st.file_uploader("请上传配置文件", type="yaml")
-
-    if uploaded_file is not None:
-        file_path = save_uploaded_file(uploaded_file)
-        if file_path:
-            config = load_config(file_path)
-            st.write("配置文件加载成功")
-
+    if config_path:
+        # 从命令行参数指定的文件路径加载配置
+        if os.path.exists(config_path):
+            config = load_config(config_path)
+            # st.write("配置文件加载成功")
             run_app(config)
-
+        else:
+            st.error(f"指定的配置文件不存在: {config_path}")
     else:
-        st.write("Please upload a config file.")
+        # 使用Streamlit界面上传配置文件
+        uploaded_file = st.file_uploader("请上传配置文件", type="yaml")
 
+        if uploaded_file is not None:
+            file_path = save_uploaded_file(uploaded_file)
+            if file_path:
+                config = load_config(file_path)
+                st.write("配置文件加载成功")
+                run_app(config)
+        else:
+            st.write("Please upload a config file.")
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Streamlit app with optional config file argument.")
+    parser.add_argument("--config", type=str, help="Path to the configuration file.")
+    
+    args = parser.parse_args()
+    
+    main(config_path=args.config)
